13bd0fcd5223403d6fbdf1fdf2bf1c90d4e35468
Pol MP <pol.moreno.puig@estudiant.upc.edu>
2017-02-16 17:07:34 +0100

    Afegit main, funció per comprovar matrícula, optimització de codi...

--- Pr1/tasca2.py
+++ Pr1/tasca2.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import os
+import string
+import sys
 
 def init():
 	f=open('places.dat','w+')
@@ -9,15 +12,18 @@ def init():
 			f.write(' ')
 	f.close()
 
-def optionsProg():
-	print "1. Ocupar plaça concreta"
-	print "2. Ocupar primera plaça buida"
-	print "3. Sortir del parquing"
-	print "4. Consultar estat"
-	print "5. Llistar places buides"
-	print "6. Comprova vehicle"
+def demanaPl():
+	try:
+		pl=input('Introdueix la plaça [0-1000]: ')
+		if pl in range(0,1001):
+			return pl
+		else:
+			return False
+	except: #No s'ha pogut convertir a int
+		return False
 
 def intMat(matricula,posicio=-1):
+	global f
 	if posicio==-1:
 		for i in range(1001):
 			f.seek(i*8)
@@ -35,6 +41,7 @@ def intMat(matricula,posicio=-1):
 			return False
 
 def delMat(matricula):
+	global f
 	for i in range(1001):
 		f.seek(i*8)
 		if f.read(7) == matricula:
@@ -44,10 +51,10 @@ def delMat(matricula):
 	return False
 
 def estatPl(posicio):
+	global f
 	f.seek(posicio*8)
 	if f.read(1) == 'X':
 		return False
-
 	else:
 		return True
 
@@ -59,6 +66,30 @@ def llistaBuides():
 			a.append(i)
 	return a
 
+def demanaMat():
+	mat=raw_input('Introdueix la matrícula: ')
+	if not comprovaMatricula(mat):
+		return False
+	else:
+		return mat
+
+def comprovaMatricula(matricula):
+	isCorrect=True
+	for numb,character in enumerate(matricula):
+		if numb < 4:
+			if character not in string.digits:
+				isCorrect=False
+				break
+		elif numb > 3 and numb < 7:
+			if character not in string.letters:
+				isCorrect=False
+				break
+		else:
+			isCorrect=False
+			break
+	return isCorrect
+
+
 def comprovaVehicle(matricula):
 	for i in range(1001):
 		f.seek(i*8)
@@ -66,75 +97,96 @@ def comprovaVehicle(matricula):
 			return i
 	return -1
 
+def optionsProg():
+	print "1. Ocupar plaça concreta"
+	print "2. Ocupar primera plaça buida"
+	print "3. Sortir del pàrquing"
+	print "4. Consultar estat de plaça"
+	print "5. Llistar places buides"
+	print "6. Comprova vehicle"
+	print "7. Sortir"
 
-			
+def main():
+	while True:
+		optionsProg()
+		opt=raw_input('Introdueix opcio: ')
+		if opt=='1':
+			print "Ocupar plaça concreta"
+			pl=demanaPl()
+			if not pl:
+				print "Plaça incorrecta!"
+			else:
+				mat=demanaMat()
+				if not mat or comprovaVehicle(mat) != -1:
+					print "Matrícula incorrecta o ja està afegida!"
+				else:
+					if intMat(mat,pl):
+						print "Matrícula afegida correctament"
+					else:
+						print "No s'ha pogut afegir la matrícula"
 
-t=raw_input('Vols inicialitzar? [s/n]')
-if t=='s':
+		elif opt=='2':
+			mat=demanaMat()
+			if not mat or comprovaVehicle(mat) != -1:
+				print "Matrícula incorrecta o ja està afegida!"
+			else:
+				if intMat(mat):
+					print "Matrícula afegida correctament"
+				else:
+					print "No s'ha pogut afegit la matrícula"
+
+		elif opt=='3':
+			mat=demanaMat()
+			if not mat:
+				print "Matrícula incorrecta!"
+			else:
+				if delMat(mat):
+					print "Matrícula eliminada correctament"
+				else:
+					print "No s'ha pogut trobar la matrícula!"
+
+		elif opt=='4':
+			pl=demanaPl()
+			if not pl:
+				print "Plaça incorrecta!"
+			else:
+				if estatPl(pl):
+					print "La plaça "+str(pl)+" està ocupada!"
+				else:
+					print "La plaça "+str(pl)+" està buida!"
+
+		elif opt=='5':
+			if len(llistaBuides()) == 0:
+				print "No hi ha llocs buits"
+			else:
+				print 'Llista de llocs buits:\n'+str(llistaBuides())
+
+		elif opt=='6':
+			mat=demanaMat()
+			if not mat:
+				print "Matrícula incorrecta"
+			else:
+				posicio=comprovaVehicle(mat)
+				if posicio != -1:
+					print "El vehicle està en la posició "+str(posicio)
+				else:
+					print "El vehicle no està al parquing"
+
+		elif opt=='7':
+			break
+		raw_input()
+	f.close()
+
+#Inicialització principal
+if not os.path.isfile('places.dat'):
 	init()
 
 f=open('places.dat','r+')
 
-while(True):
-	optionsProg()
-	a=raw_input('Introdueix opcio: ')
-	if a=='1':
-		print "Quina plaça vols?"
-		try:
-			selparquing=input()
-		except:
-			print "No es un integer!"
-		else:
-			options=range(0,1001)
-			if selparquing in options:
-				matricula=raw_input('Fica la matricula: ')
-				if len(matricula) == 7:
-					if intMat(matricula,selparquing):
-						print "Matricula afegida correctament"
-					else:
-						print "No s'ha pogut afegit la matricula"
-	elif a=='2':
-		matricula=raw_input('Fica la matricula: ')
-		if len(matricula) == 7:
-			if intMat(matricula):
-				print "Matricula afegida correctament"
-			else:
-				print "No s'ha pogut afegit la matricula"
+try:
+	main()
+except KeyboardInterrupt:
+	print "\nTancant programa..."
+	f.close()
+	sys.exit(1)
 
-	elif a=='3':
-		matricula=raw_input('Introdueix matricula: ')
-		if len(matricula) == 7:
-			if delMat(matricula):
-				print "Matricula eliminada correctament"
-			else:
-				print "No s'ha pogut eliminar la matricula"
-
-	elif a=='4':
-		try:
-			selparquing=input("Numero de plaça: ")
-		except:
-			print "No es un integer!"
-		else:
-			options=range(0,1001)
-			if selparquing in options:
-				if estatPl(selparquing):
-					print "Està ocupat"
-				else:
-					print "Està buit"
-
-	elif a=='5':
-		if len(llistaBuides()) == 0:
-			print "No hi ha llocs buits"
-		else:
-			print 'Llista de llocs buits:\n'+str(llistaBuides())
-
-	elif a=='6':
-		matricula=raw_input('Fica la matricula: ')
-		if len(matricula) == 7:
-			posicio=comprovaVehicle(matricula)
-			if posicio != -1:
-				print "El vehicle està en la posició "+str(posicio)
-			else:
-				print "El vehicle no està al parquing"
-
-f.close()
