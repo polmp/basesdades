5f7469f5f401fe29d70bbb3ca0113e3c487582d2
Pol MP <pol.moreno.puig@estudiant.upc.edu>
2017-02-22 09:10:21 +0100

    Fet 1-2 opcions de la tasca 4

---
+++ Pr1/tasca4.py
@@ -0,0 +1,230 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import os
+import string
+import sys
+import struct
+
+def init(nameinfo):
+    global formatStruct
+    global sizeMat
+    global sizeMarca
+    global sizeCol
+    f=open(nameinfo,'w+')
+    for i in range(0,1001):
+        f.write('X'*sizeMat+'0'*sizeMarca+'0'*sizeCol)
+        if i != 1000:
+            f.write(' ')
+    f.close()
+
+def demanaPl():
+	try:
+		pl=input('Introdueix la plaça [0-1000]: ')
+		if pl in range(0,1001):
+			return pl
+		else:
+			return False
+	except: #No s'ha pogut convertir a int
+		return False
+
+def intMat(matricula,carcotxe,posicio=-1):
+    global f
+    global formatStruct
+    emptyMat = 'XXXXXXX'
+    if posicio==-1:
+        for i in range(1001):
+            f.seek(i*(formatStruct.size+1))
+            values=formatStruct.unpack(f.read(24))
+            if values[0] == emptyMat:
+                f.seek(i*(formatStruct.size+1))
+                f.write(matricula+carcotxe[0]+carcotxe[1])
+                return True
+
+    else:
+        f.seek(posicio*(formatStruct.size+1))
+        values=formatStruct.unpack(f.read(24))
+        if values[0] == emptyMat:
+            f.seek(posicio*(formatStruct.size+1))
+            f.write(matricula+carcotxe[0]+carcotxe[1])
+            return True
+        else:
+            return False
+
+def delMat(matricula):
+	global f
+	for i in range(1001):
+		f.seek(i*8)
+		if f.read(7) == matricula:
+			f.seek(i*8)
+			f.write('XXXXXXX')
+			return True
+	return False
+
+def estatPl(posicio):
+	global f
+	f.seek(posicio*8)
+	if f.read(1) == 'X':
+		return False
+	else:
+		return True
+
+
+def llistaBuides():
+	a=[]
+	for i in range(1001):
+		if not estatPl(i):
+			a.append(i)
+	return a
+
+def demanaMat():
+	mat=raw_input('Introdueix la matrícula: ')
+	if not comprovaMatricula(mat):
+		return False
+	else:
+		return mat
+
+def demanaColMarca():
+    global sizeMarca
+    global sizeCol
+    marccotxe=raw_input("Escriu la marca del cotxe: ")
+    while len(marccotxe) < sizeMarca:
+        marccotxe+='0'
+    colcotxe=raw_input("Escriu el color del cotxe: ")
+    while len(colcotxe) < sizeCol:
+        colcotxe+='0'
+
+    return [colcotxe, marccotxe] if (colcotxe[:colcotxe.find('0')][:sizeCol].isalpha() and len(colcotxe) <= sizeMarca) and (marccotxe[:marccotxe.find('0')][:sizeMarca].isalpha() and len(marccotxe) <= sizeMarca) else False
+
+def comprovaMatricula(matricula):
+	isCorrect=True
+	for numb,character in enumerate(matricula):
+		if numb < 4:
+			if character not in string.digits:
+				isCorrect=False
+				break
+		elif numb > 3 and numb < 7:
+			if character not in string.letters:
+				isCorrect=False
+				break
+		else:
+			isCorrect=False
+			break
+	return isCorrect
+
+
+def comprovaVehicle(matricula):
+    global f
+    for i in range(1001):
+        f.seek(i*(formatStruct.size+1))
+        values=formatStruct.unpack(f.read(24))
+        if values[0] == matricula:
+            return i
+    return -1
+
+def optionsProg():
+	print "1. Ocupar plaça concreta"
+	print "2. Ocupar primera plaça buida"
+	print "3. Sortir del pàrquing"
+	print "4. Consultar estat de plaça"
+	print "5. Llistar places buides"
+	print "6. Comprova vehicle"
+	print "7. Sortir"
+
+def main():
+    while True:
+        optionsProg()
+        opt=raw_input('Introdueix opcio: ')
+        if opt=='1':
+            print "Ocupar plaça concreta"
+            pl=demanaPl()
+            if not pl:
+                print "Plaça incorrecta"
+            else:
+                mat=demanaMat()
+                if not mat or comprovaVehicle(mat) != -1:
+                    print "Matrícula incorrecta o ja està afegida!"
+                else:
+                    carcotxe=demanaColMarca()
+                    if not carcotxe:
+                        print "Paràmetres incorrectes!"
+                    else:
+                        if intMat(mat,carcotxe,pl):
+                            print "Cotxe afegit correctament"
+                        else:
+                            print "No s'ha pogut afegir el cotxe"
+
+        elif opt=='2':
+            mat=demanaMat()
+            if not mat or comprovaVehicle(mat) != -1:
+                print "Matrícula incorrecta o ja està afegida!"
+            else:
+                carcotxe=demanaColMarca()
+                if not carcotxe:
+                    print "Paràmetres incorrectes!"
+                else:
+                    if intMat(mat,carcotxe):
+                        print "Cotxe afegit correctament"
+                    else:
+                        print "No s'ha pogut afegir el cotxe"
+
+        elif opt=='3':
+			mat=demanaMat()
+			if not mat:
+				print "Matrícula incorrecta!"
+			else:
+				if delMat(mat):
+					print "Matrícula eliminada correctament"
+				else:
+					print "No s'ha pogut trobar la matrícula!"
+
+        elif opt=='4':
+			pl=demanaPl()
+			if not pl:
+				print "Plaça incorrecta!"
+			else:
+				if estatPl(pl):
+					print "La plaça "+str(pl)+" està ocupada!"
+				else:
+					print "La plaça "+str(pl)+" està buida!"
+
+        elif opt=='5':
+			if len(llistaBuides()) == 0:
+				print "No hi ha llocs buits"
+			else:
+				print 'Llista de llocs buits:\n'+str(llistaBuides())
+
+        elif opt=='6':
+			mat=demanaMat()
+			if not mat:
+				print "Matrícula incorrecta"
+			else:
+				posicio=comprovaVehicle(mat)
+				if posicio != -1:
+					print "El vehicle està en la posició "+str(posicio)
+				else:
+					print "El vehicle no està al parquing"
+
+        elif opt=='7':
+			break
+        raw_input()
+    f.close()
+
+saveInfoname='places2.dat'
+
+sizeMat=7
+sizeMarca=10
+sizeCol=7
+formatStruct=struct.Struct(str(sizeMat)+'s '+str(sizeMarca)+'s '+str(sizeCol)+'s') #Definim un format on -> Matricula -> 7 caràcters | Marca -> 10 caràcters | Color -> 7 caràcters
+
+#Inicialització principal
+if not os.path.isfile(saveInfoname):
+    init(saveInfoname)
+
+f=open(saveInfoname,'r+')
+
+try:
+	main()
+except KeyboardInterrupt:
+	print "\nTancant programa..."
+	f.close()
+	sys.exit(1)
