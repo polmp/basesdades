f6ccd020c7cf06fd5cd96994c27a8d2ab5c35e44
_ <_@_.com>
2017-03-15 15:02:12 +0100

    sensors

--- Pr2/sensorsCreation.sql
+++ Pr2/sensorsCreation.sql
@@ -44,21 +44,30 @@ INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES (DATETI
 
 INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 20:20:10',653,3,556,0,300);
 INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 20:59:11',683,3,557,2,0);
-INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:00:00',712,3,558,4,420);
-INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:40:23',715,3,560,10,478);
-INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 18:42:28',725,3,562,12,499);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:00:00',712,3,558,4,120);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:40:23',715,3,560,10,278);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 18:42:28',725,3,562,12,479);
 
 INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 20:20:10',653,1,556,0,300);
 INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 20:59:11',683,1,557,2,0);
-INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:00:00',712,1,558,4,420);
-INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:40:23',715,1,560,10,478);
-INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 18:42:28',725,1,562,12,499);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:00:00',712,1,558,4,220);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:40:23',715,1,560,10,278);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 18:42:28',725,1,562,12,399);
+
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 20:20:10',653,2,556,0,300);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 20:59:11',683,2,557,2,0);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:00:00',712,2,558,4,220);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 21:40:23',715,2,560,10,278);
+INSERT INTO sensors (result_time,epoch,nodeid,light,temp,voltage) VALUES ('2015-03-05 18:42:28',725,2,962,12,199);
 
 
 
 CREATE TABLE calib_temp as select temp, avg(temp)+temp as calib from sensors group by temp;
 CREATE TABLE calib_light as select light, avg(light)+light as calib from sensors group by light;
-
+/*
+SELECT * FROM calib_light;
+SELECT * FROM calib_temp;
+*/
 
 -- 1. rite a query (using the SELECT statement) that will compute times
 -- and ids when any sensor’s light reading was above 550. Show both the query 
@@ -80,15 +89,41 @@ CREATE TABLE calib_light as select light, avg(light)+light as calib from sensors
 							OR ((cast( (strftime('%H',result_time)) as int)) = 21 AND (cast( (strftime('%M',result_time)) as int)) = 0) AND (cast( (strftime('%S',result_time)) as int)) = 00);
 */
 
-SELECT avg(light) from sensors WHERE ((( (cast( (strftime('%H',result_time)) as int)) BETWEEN 18 AND 20)
+--AVERAGE LIGHT AND TEMP, GROUP BY NODE
+/*
+SELECT avg(light), avg(temp), nodeid from sensors WHERE ((( (cast( (strftime('%H',result_time)) as int)) BETWEEN 18 AND 20)
 							OR ((cast( (strftime('%H',result_time)) as int)) = 21 AND (cast( (strftime('%M',result_time)) as int)) = 0) AND (cast( (strftime('%S',result_time)) as int)) = 00)
-							AND nodeid = 1
-							);
+							) GROUP BY nodeid;
+*/
+-- 3. Write a single query that computes the average temperature and light reading at every
+-- sensor between 6 PM and 9 PM, but exclude any sensors whose maximum voltage was
+-- greater than 418 during that time period. Show both the query and the result.
+
+SELECT *, light,temp, nodeid, voltage, max(voltage) from sensors WHERE ((( (cast( (strftime('%H',result_time)) as int)) BETWEEN 18 AND 20)
+							OR ((cast( (strftime('%H',result_time)) as int)) = 21 AND (cast( (strftime('%M',result_time)) as int)) = 0) AND (cast( (strftime('%S',result_time)) as int)) = 00)
+							) GROUP BY nodeid 
+							;--EXCEPT SELECT avg(light), avg(temp), nodeid, max(voltage) FROM sensors WHERE voltage > 418 GROUP BY nodeid;
 
 
-							 --AND MINUTS = 0 AND SEGONS = 0)); 
---SELECT * FROM (SELECT strftime('%M',result_time) from sensors); 
+--SELECT avg(light), avg(temp), nodeid, max(voltage) FROM sensors WHERE voltage > 418 GROUP BY nodeid;
 
---SELECT (CONVERT(VARCHAR(8), result_time, 108)) from sensors; 
-						--(SELECT DatePart(HOUR,result_time) BETWEEN 18 AND 21);
-						-- AND result_time(MINUTE, SyncDate) BETWEEN 0 AND 30
+--SELECT * FROM sensors GROUP BY nodeid EXCEPT SELECT * FROM sensors WHERE LIGHT > 800;
+
+
+
+
+
+-- 4. Write a query that computes the average calibrated temperature readings from sensor 2 during each hour, inclusive, between 6 PM and 9 PM (i.e., your
+-- answer should consist of 4 rows of calibrated temperatures.)
+
+SELECT *, light,temp, nodeid, voltage/*, max(voltage)*/ from sensors WHERE ((( (cast( (strftime('%H',result_time)) as int)) BETWEEN 18 AND 20)
+							OR ((cast( (strftime('%H',result_time)) as int)) = 21 AND (cast( (strftime('%M',result_time)) as int)) = 0) AND (cast( (strftime('%S',result_time)) as int)) = 00)
+							);-- GROUP BY nodeid 
+
+
+
+
+
+
+
+
