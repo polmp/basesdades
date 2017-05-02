256f61ef6763ba4c9fe86b7d17148032dc8197ca
Pol MP <pol.moreno.puig@estudiant.upc.edu>
2017-05-02 20:46:58 +0200

    Afegit minim distancia sql

--- Pr4/taula_mobil.sql
+++ Pr4/taula_mobil.sql
@@ -1,8 +1,9 @@
+
 CREATE TABLE IF NOT EXISTS cobertura_mobil(
 	time_stamp timestamp,
 	data datetime,
-	lat REAL,
-	lng REAL,
+	lat real,
+	lng real,
 	signal_inst INT,
 	signal_min INT,
 	signal_max INT,
@@ -42,8 +43,14 @@ CREATE TABLE IF NOT EXISTS long_lat (
 .import csv/data_export_03-2017.csv cobertura_mobil
 .import csv/listado-longitud-latitud-municipios-espana.csv long_lat
 
+.mode column
+.header on
 DELETE from cobertura_mobil where cid like '%cid%';
 
 DELETE from long_lat where longitud like '%Longitud%';
+CREATE VIEW cb as SELECT lat AS LAT_COB,lng AS LONG_COB FROM cobertura_mobil order by time_stamp limit 5;
+CREATE VIEW pob as SELECT poblacio,latitud AS LAT_POB,longitud AS LONG_POB FROM long_lat WHERE comunitat like "catalu%";
 
+SELECT *,abs(abs(cb.LAT_COB-pob.LAT_POB)-abs(cb.LONG_COB-pob.LONG_POB)) as Distancia from cb,pob order by Distancia;
+SELECT *,min(abs(abs(cb.LAT_COB-pob.LAT_POB)-abs(cb.LONG_COB-pob.LONG_POB))) as Distancia from cb,pob group by cb.LAT_COB,cb.LONG_COB order by Distancia ASC;
 
