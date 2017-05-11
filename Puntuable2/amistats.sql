bf9369bc0bce9afc771d8d84529a8f95dc109f88
_ <_@_.com>
2017-05-11 02:28:52 +0200

    millorat amistats

--- Puntuable2/amistats.sql
+++ Puntuable2/amistats.sql
@@ -66,42 +66,58 @@ END;
 
 /*Tasca 2: Escriure un o més triggers per gestionar el grau dels nous usuaris. Si el registre inserit és 
 inferior a 9 o major a 12, cal canviar el valor a NULL. Addicionalment, si el registre inserit té un valor null a grau, cal canviar-lo per 9.*/
-CREATE TRIGGER IF NOT EXISTS canvia_grau_null AFTER INSERT ON USUARIS WHEN NEW.grau < 9 or NEW.grau > 12 BEGIN
-UPDATE usuaris set grau = null WHERE id = NEW.id;
+CREATE TRIGGER IF NOT EXISTS canvia_grau_null AFTER INSERT ON USUARIS 
+	WHEN NEW.grau < 9 or NEW.grau > 12 
+BEGIN
+	UPDATE usuaris set grau = null WHERE id = NEW.id;
 END;
 
-CREATE TRIGGER IF NOT EXISTS canvia_grau_9 AFTER INSERT ON USUARIS WHEN NEW.grau is null BEGIN
-UPDATE usuaris set grau = 9 WHERE id = NEW.id;
+CREATE TRIGGER IF NOT EXISTS canvia_grau_9 AFTER INSERT ON USUARIS 
+	WHEN NEW.grau is null 
+BEGIN
+	UPDATE usuaris set grau = 9 WHERE id = NEW.id;
 END;
 
 /*Tasca 3: Escriure un o més triggers que mantinguin la relació de simetria en amics. 
 Específicament, si s'elimina (A,B) de amistats, aleshores també cal eliminar (B,A). Si s'insereix (A,B), també cal inserir (B,A). 
 No cal mantenir modificacions.*/
 
-CREATE TRIGGER IF NOT EXISTS afegeix_simetria AFTER INSERT ON amistats BEGIN
-INSERT OR IGNORE INTO amistats values (NEW.ID2,NEW.ID1);
+CREATE TRIGGER IF NOT EXISTS afegeix_simetria AFTER INSERT ON amistats 
+BEGIN
+	INSERT OR IGNORE INTO amistats values (NEW.ID2,NEW.ID1);
 END;
 
-CREATE TRIGGER IF NOT EXISTS borra_simetria DELETE ON amistats BEGIN
-DELETE FROM amistats where ID1=OLD.ID2 and ID2=OLD.ID1;
+CREATE TRIGGER IF NOT EXISTS borra_simetria DELETE ON amistats 
+BEGIN
+	DELETE FROM amistats where ID1=OLD.ID2 and ID2=OLD.ID1;
 END;
 
 /*Tasca 4: Escriure un trigger que automàticament esborri estudiants quan aquests es graduin, és a dir, quan el grau superi el valor 12.*/
-CREATE TRIGGER IF NOT EXISTS borra_estudiant UPDATE ON usuaris WHEN NEW.grau > 12 BEGIN
-DELETE FROM usuaris WHERE NEW.grau > 12 and id=NEW.id;
+CREATE TRIGGER IF NOT EXISTS borra_estudiant UPDATE ON usuaris 
+	WHEN NEW.grau > 12 
+BEGIN
+	DELETE FROM usuaris WHERE NEW.grau > 12 and id=NEW.id;
 END;
 
 /*Tasca 5: Escriure un trigger tal que quan un usuari incrementi un grau, també ho facin els seus amics.*/
-CREATE TRIGGER IF NOT EXISTS incrementa_grau AFTER UPDATE ON usuaris WHEN NEW.grau = OLD.grau+1 BEGIN
-UPDATE usuaris set grau=grau+1 WHERE ID IN (SELECT ID1 from amistats where ID2 = NEW.ID);
+CREATE TRIGGER IF NOT EXISTS incrementa_grau AFTER UPDATE ON usuaris 
+	WHEN NEW.grau = OLD.grau+1 
+BEGIN
+	UPDATE usuaris set grau=grau+1 
+		WHERE ID IN 
+			(SELECT ID1 from amistats where ID2 = NEW.ID);
 END;
 
 /*Tasca 6: Escriure un trigger que forci el següent comportament: Si A té preferència per B, però es modifica a que A té preferència per C,
 i B i C són amics, aleshores B i C no poden ser amics. Per tant cal eliminar la relació d'amistat en els 2 sentits (B,C) i (C,B).*/
 
 
-CREATE TRIGGER IF NOT EXISTS borra_amics_preferencia BEFORE UPDATE ON preferencies BEGIN
-DELETE FROM amistats where (ID1=OLD.ID2 and ID2=NEW.ID2) OR (ID1=NEW.ID2 and ID2=OLD.ID2);
+CREATE TRIGGER IF NOT EXISTS borra_amics_preferencia BEFORE UPDATE ON preferencies 
+for each row
+BEGIN
+	--DELETE FROM amistats where (ID1=OLD.ID2 and ID2=NEW.ID2) OR (ID1=NEW.ID2 and ID2=OLD.ID2);
+	delete from amistats where ID1=(select new.ID2_2 from preferencies where old.ID2_2<>new.ID2_2) and 
+		ID2=(select old.ID2_2 from preferencies where old.ID2_2<>new.ID2_2);
 END;
 
 /*Prova 6*/
